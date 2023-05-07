a6037300f25f754bc3bd911c42e6747e549954fe
altescy <altescy@fastmail.com>
2023-05-07 18:41:02 +0900

    add multilabel accuracy

--- nlpstack/torch/metrics/accuracy.py
+++ nlpstack/torch/metrics/accuracy.py
@@ -45,6 +45,41 @@ class Accuracy(ClassificationMetric):
         self._total_count = 0.0
 
 
+class MultilabelAccuracy(MultilabelClassificationMetric):
+    def __init__(self, threshold: float = 0.5) -> None:
+        self._threshold = threshold
+        self._num_correct = 0.0
+        self._num_total = 0.0
+
+    def __call__(  # type: ignore[override]
+        self,
+        pred: torch.LongTensor | torch.FloatTensor,
+        gold: torch.LongTensor,
+    ) -> None:
+        """
+        :param pred: (batch_size, num_classes)
+        :param gold: (batch_size, num_classes)
+        """
+        pred, gold = self.detach_tensors(pred, gold)  # type: ignore[assignment]
+        if pred.dtype in (torch.float, torch.double):
+            pred = cast(torch.LongTensor, (pred > self._threshold).long())
+
+        self._num_correct += (pred == gold).all(dim=1).sum().item()
+        self._num_total += pred.size(0)
+
+    def get_metrics(self, reset: bool = False) -> dict[str, float]:
+        if self._num_total == 0:
+            return {"accuracy": 0.0}
+        metrics = {"accuracy": self._num_correct / self._num_total}
+        if reset:
+            self.reset()
+        return metrics
+
+    def reset(self) -> None:
+        self._num_correct = 0.0
+        self._num_total = 0.0
+
+
 class OverallAccuracy(MultilabelClassificationMetric):
     def __init__(self, threshold: float = 0.5) -> None:
         self._threshold = threshold
