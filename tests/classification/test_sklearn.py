296be6f74f9f1ff21c90080035f20d8eff344c43
altescy <altescy@fastmail.com>
2023-05-25 00:22:33 +0900

    modify test config

--- tests/classification/test_sklearn.py
+++ tests/classification/test_sklearn.py
@@ -10,7 +10,7 @@ def test_basic_classifier() -> None:
     ]
     y = ["positive", "negative", "positive", "negative"]
 
-    classifier = BasicClassifier(max_epochs=16)
+    classifier = BasicClassifier(max_epochs=16, learning_rate=1e-2)
     classifier.fit(X, y)
 
     predictions = classifier.predict(X)
