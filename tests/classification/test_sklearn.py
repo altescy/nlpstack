cb3cb698319a84ca44af3899d85683599e762872
altescy <altescy@fastmail.com>
2023-05-25 00:15:34 +0900

    add test for classification model

---
+++ tests/classification/test_sklearn.py
@@ -0,0 +1,18 @@
+from nlpstack.classification.sklearn import BasicClassifier
+
+
+def test_basic_classifier() -> None:
+    X = [
+        "this is a positive example",
+        "this is a negative example",
+        "this is a positive example",
+        "this is a negative example",
+    ]
+    y = ["positive", "negative", "positive", "negative"]
+
+    classifier = BasicClassifier(max_epochs=8)
+    classifier.fit(X, y)
+
+    predictions = classifier.predict(X)
+
+    assert [pred.label for pred in predictions] == y
