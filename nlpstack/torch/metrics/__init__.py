4817a8b8b4ad60d8039127c9c6a8adc61a0672bd
altescy <altescy@fastmail.com>
2023-05-27 02:32:54 +0900

    remove torch metrics

--- nlpstack/torch/metrics/__init__.py
+++
@@ -1,3 +0,0 @@
-from nlpstack.torch.metrics.accuracy import Accuracy, AverageAccuracy, MultilabelAccuracy, OverallAccuracy  # noqa: F401
-from nlpstack.torch.metrics.fbeta import MacroMultilabelFBeta, MicroMultilabelFBeta  # noqa: F401
-from nlpstack.torch.metrics.metric import ClassificationMetric, Metric, MultilabelClassificationMetric  # noqa: F401
