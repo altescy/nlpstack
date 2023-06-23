144762572b32402b074cd66f968c1def665a4957
altescy <altescy@fastmail.com>
2023-06-24 03:44:44 +0900

    add io modules & label encondig utils

---
+++ nlpstack/tasks/sequence_labeling/io.py
@@ -0,0 +1,114 @@
+import itertools
+import json
+from typing import Iterable, Iterator, Literal, Optional, Sequence
+
+import minato
+
+from nlpstack.data import Token
+
+from .data import SequenceLabelingExample, SequenceLabelingPrediction
+from .util import LabelEncoding, convert_encoding
+
+
+class JsonlReader:
+    def __init__(
+        self,
+        text_field: str = "text",
+        label_field: str = "labels",
+    ) -> None:
+        self.text_field = text_field
+        self.label_field = label_field
+
+    def __call_(self, fielname: str) -> Iterator[SequenceLabelingExample]:
+        with minato.open(fielname) as jsonlfile:
+            for line in jsonlfile:
+                example = json.loads(line)
+                yield SequenceLabelingExample(
+                    text=example[self.text_field],
+                    labels=example.get(self.label_field),
+                )
+
+
+class JsonlWriter:
+    def __init__(
+        self,
+        additional_fields: Optional[Sequence[str]] = None,
+    ) -> None:
+        self.additional_fields = additional_fields or []
+
+    def __call__(
+        self,
+        filename: str,
+        predictions: Iterable[SequenceLabelingPrediction],
+    ) -> None:
+        with minato.open(filename, "w") as jsonlfile:
+            for prediction in predictions:
+                output = {"tokens": prediction.tokens, "labels": prediction.labels}
+                for field in self.additional_fields:
+                    if prediction.metadata is None:
+                        raise ValueError("metadata is not available")
+                    if field not in prediction.metadata:
+                        raise ValueError(f"metadata does not have {field} field")
+                    output[field] = prediction.metadata[field]
+                jsonlfile.write(json.dumps(output, ensure_ascii=False) + "\n")
+
+
+class Conll2003Reader:
+    def __init__(
+        self,
+        label_field: Literal["ner", "pos", "chunk"],
+        label_encoding: LabelEncoding = "IOB1",
+        convert_to_label_encoding: Optional[LabelEncoding] = None,
+        use_postag_as_feature: bool = False,
+    ) -> None:
+        self.label_field = label_field
+        self.label_encoding = label_encoding
+        self.convert_to_label_encoding = convert_to_label_encoding
+        self.use_postag_as_feature = use_postag_as_feature
+
+    @staticmethod
+    def _is_divider(line: str) -> bool:
+        empty_line = line.strip() == ""
+        if empty_line:
+            return True
+        else:
+            first_token = line.split()[0]
+            if first_token == "-DOCSTART-":
+                return True
+            else:
+                return False
+
+    def __call__(self, filename: str) -> Iterator[SequenceLabelingExample]:
+        with minato.open(filename) as conllfile:
+            line_chunks = (
+                line for is_divider, line in itertools.groupby(conllfile, self._is_divider) if not is_divider
+            )
+            for lines in line_chunks:
+                fields = [list(field) for field in zip(*(line.strip().split() for line in lines))]
+                _tokens, pos_tags, chunk_tags, ner_tags = fields
+
+                if self.use_postag_as_feature:
+                    tokens = list(itertools.starmap(Token, zip(_tokens, pos_tags)))
+                else:
+                    tokens = [Token(surface) for surface in _tokens]
+
+                if self.label_field == "ner":
+                    labels = ner_tags
+                elif self.label_field == "pos":
+                    labels = pos_tags
+                elif self.label_field == "chunk":
+                    labels = chunk_tags
+                else:
+                    raise ValueError(f"Unknown label field: {self.label_field}")
+
+                if self.convert_to_label_encoding is not None:
+                    labels = convert_encoding(
+                        labels,
+                        source_encoding=self.label_encoding,
+                        target_encoding=self.convert_to_label_encoding,
+                    )
+
+                yield SequenceLabelingExample(
+                    text=tokens,
+                    labels=labels,
+                )
--- nlpstack/tasks/sequence_labeling/util.py
+++ nlpstack/tasks/sequence_labeling/util.py
@@ -1,6 +1,8 @@
-from typing import List, Optional, Sequence, Set, Tuple
+import itertools
+from typing import List, Literal, Optional, Sequence, Tuple
 
 LabelWithSpan = Tuple[str, Tuple[int, int]]
+LabelEncoding = Literal["BIO", "IOB1", "BIOUL", "BMES"]
 
 
 class InvalidTagSequence(Exception):
@@ -14,175 +16,263 @@ class InvalidTagSequence(Exception):
         return " ".join(self.tag_sequence)
 
 
-def bio_tags_to_spans(
-    tag_sequence: Sequence[str],
-    classes_to_ignore: Optional[Sequence[str]] = None,
-) -> List[LabelWithSpan]:
-    classes_to_ignore = classes_to_ignore or []
-    spans: Set[Tuple[str, Tuple[int, int]]] = set()
-    span_start = 0
-    span_end = 0
-    active_conll_tag: Optional[str] = None
-    for index, string_tag in enumerate(tag_sequence):
-        # Actual BIO tag.
-        bio_tag = string_tag[0]
-        if bio_tag not in ("B", "I", "O"):
-            raise InvalidTagSequence(tag_sequence)
-        conll_tag = string_tag[2:]
-        if bio_tag == "O" or conll_tag in classes_to_ignore:
-            # The span has ended.
-            if active_conll_tag is not None:
-                spans.add((active_conll_tag, (span_start, span_end)))
-            active_conll_tag = None
-            # We don't care about tags we are
-            # told to ignore, so we do nothing.
-            continue
-        elif bio_tag == "B":
-            # We are entering a new span; reset indices
-            # and active tag to new span.
-            if active_conll_tag is not None:
-                spans.add((active_conll_tag, (span_start, span_end)))
-            active_conll_tag = conll_tag
-            span_start = index
-            span_end = index
-        elif bio_tag == "I" and conll_tag == active_conll_tag:
-            # We're inside a span.
-            span_end += 1
-        else:
-            # This is the case the bio label is an "I", but either:
-            # 1) the span hasn't started - i.e. an ill formed span.
-            # 2) The span is an I tag for a different conll annotation.
-            # We'll process the previous span if it exists, but also
-            # include this span. This is important, because otherwise,
-            # a model may get a perfect F1 score whilst still including
-            # false positive ill-formed spans.
-            if active_conll_tag is not None:
-                spans.add((active_conll_tag, (span_start, span_end)))
-            active_conll_tag = conll_tag
-            span_start = index
-            span_end = index
-    # Last token might have been a part of a valid span.
-    if active_conll_tag is not None:
-        spans.add((active_conll_tag, (span_start, span_end)))
-    return list(spans)
-
-
-def iob1_tags_to_spans(
-    tag_sequence: Sequence[str],
-    classes_to_ignore: Optional[Sequence[str]] = None,
-) -> List[LabelWithSpan]:
-    def _iob1_start_of_chunk(
-        prev_bio_tag: Optional[str],
-        prev_conll_tag: Optional[str],
-        curr_bio_tag: str,
-        curr_conll_tag: str,
-    ) -> bool:
-        if curr_bio_tag == "B":
-            return True
-        if curr_bio_tag == "I" and prev_bio_tag == "O":
-            return True
-        if curr_bio_tag != "O" and prev_conll_tag != curr_conll_tag:
-            return True
-        return False
-
-    classes_to_ignore = classes_to_ignore or []
-    spans: Set[Tuple[str, Tuple[int, int]]] = set()
-    span_start = 0
-    span_end = 0
-    active_conll_tag: Optional[str] = None
-    prev_bio_tag: Optional[str] = None
-    prev_conll_tag: Optional[str] = None
-    for index, string_tag in enumerate(tag_sequence):
-        curr_bio_tag = string_tag[0]
-        curr_conll_tag = string_tag[2:]
-
-        if curr_bio_tag not in ("B", "I", "O"):
-            raise InvalidTagSequence(tag_sequence)
-        if curr_bio_tag == "O" or curr_conll_tag in classes_to_ignore:
-            # The span has ended.
-            if active_conll_tag is not None:
-                spans.add((active_conll_tag, (span_start, span_end)))
-            active_conll_tag = None
-        elif _iob1_start_of_chunk(prev_bio_tag, prev_conll_tag, curr_bio_tag, curr_conll_tag):
-            # We are entering a new span; reset indices
-            # and active tag to new span.
-            if active_conll_tag is not None:
-                spans.add((active_conll_tag, (span_start, span_end)))
-            active_conll_tag = curr_conll_tag
-            span_start = index
-            span_end = index
-        else:
-            # bio_tag == "I" and curr_conll_tag == active_conll_tag
-            # We're continuing a span.
-            span_end += 1
-
-        prev_bio_tag = string_tag[0]
-        prev_conll_tag = string_tag[2:]
-    # Last token might have been a part of a valid span.
-    if active_conll_tag is not None:
-        spans.add((active_conll_tag, (span_start, span_end)))
-    return list(spans)
-
-
 def bioul_tags_to_spans(
-    tag_sequence: Sequence[str],
+    tags: Sequence[str],
     classes_to_ignore: Optional[Sequence[str]] = None,
 ) -> List[LabelWithSpan]:
     spans = []
     classes_to_ignore = classes_to_ignore or []
-    index = 0
-    while index < len(tag_sequence):
-        label = tag_sequence[index]
-        if label[0] == "U":
-            spans.append((label.partition("-")[2], (index, index)))
-        elif label[0] == "B":
-            start = index
-            while label[0] != "L":
-                index += 1
-                if index >= len(tag_sequence):
-                    raise InvalidTagSequence(tag_sequence)
-                label = tag_sequence[index]
-                if not label[0] in ("I", "L"):
-                    raise InvalidTagSequence(tag_sequence)
-            spans.append((label.partition("-")[2], (start, index)))
+    for index, tag in enumerate(tags):
+        if tag == "O":
+            continue
+
+        split = tag.split("-", 1)
+        if len(split) != 2:
+            raise InvalidTagSequence(tags)
+
+        prefix, label = split
+        if label in classes_to_ignore:
+            continue
+
+        if prefix in ("B", "U"):
+            spans.append((label, (index, index)))
+        elif prefix in ("I", "L"):
+            if not spans or spans[-1][0] != label:
+                raise InvalidTagSequence(tags)
+            spans[-1] = (label, (spans[-1][1][0], index))
         else:
-            if label != "O":
-                raise InvalidTagSequence(tag_sequence)
-        index += 1
-    return [span for span in spans if span[0] not in classes_to_ignore]
+            raise InvalidTagSequence(tags)
+    return spans
+
+
+def bio_tags_to_spans(
+    tags: Sequence[str],
+    classes_to_ignore: Optional[Sequence[str]] = None,
+) -> List[LabelWithSpan]:
+    return bioul_tags_to_spans(bio_to_bioul(tags), classes_to_ignore)
 
 
 def bmes_tags_to_spans(
-    tag_sequence: Sequence[str],
+    tags: Sequence[str],
     classes_to_ignore: Optional[Sequence[str]] = None,
 ) -> List[LabelWithSpan]:
-    def extract_bmes_tag_label(text: str) -> Tuple[str, str]:
-        bmes_tag = text[0]
-        label = text[2:]
-        return bmes_tag, label
+    return bioul_tags_to_spans(bmes_to_bioul(tags), classes_to_ignore)
 
-    spans: List[Tuple[str, List[int]]] = []
-    prev_bmes_tag: Optional[str] = None
-    for index, tag in enumerate(tag_sequence):
-        bmes_tag, label = extract_bmes_tag_label(tag)
-        if bmes_tag in ("B", "S"):
-            # Regardless of tag, we start a new span when reaching B & S.
-            spans.append((label, [index, index]))
-        elif bmes_tag in ("M", "E") and prev_bmes_tag in ("B", "M") and spans[-1][0] == label:
-            # Only expand the span if
-            # 1. Valid transition: B/M -> M/E.
-            # 2. Matched label.
-            spans[-1][1][1] = index
+
+def iob1_tags_to_spans(
+    tags: Sequence[str],
+    classes_to_ignore: Optional[Sequence[str]] = None,
+) -> List[LabelWithSpan]:
+    return bioul_tags_to_spans(iob1_to_bioul(tags), classes_to_ignore)
+
+
+def spans_to_bioul_tags(spans: Sequence[LabelWithSpan], length: int) -> List[str]:
+    tags = ["O"] * length
+    for label, (start, end) in spans:
+        if start == end:
+            tags[start] = f"U-{label}"
         else:
-            # Best effort split for invalid span.
-            spans.append((label, [index, index]))
-        # update previous BMES tag.
-        prev_bmes_tag = bmes_tag
+            tags[start] = f"B-{label}"
+            tags[end] = f"L-{label}"
+            for i in range(start + 1, end):
+                tags[i] = f"I-{label}"
+    return tags
 
-    classes_to_ignore = classes_to_ignore or []
-    return [
-        # to tuple.
-        (span[0], (span[1][0], span[1][1]))
-        for span in spans
-        if span[0] not in classes_to_ignore
-    ]
+
+def spans_to_bio_tags(spans: Sequence[LabelWithSpan], length: int) -> List[str]:
+    return bioul_to_bio(spans_to_bioul_tags(spans, length))
+
+
+def spans_to_bmes_tags(spans: Sequence[LabelWithSpan], length: int) -> List[str]:
+    return bioul_to_bmes(spans_to_bioul_tags(spans, length))
+
+
+def spans_to_iob1_tags(spans: Sequence[LabelWithSpan], length: int) -> List[str]:
+    return bioul_to_iob1(spans_to_bioul_tags(spans, length))
+
+
+def iob1_to_bioul(tags: Sequence[str]) -> List[str]:
+    def convert(preceding: str, current: str, following: str) -> str:
+        if current == "O":
+            return "O"
+
+        if current.startswith("B-"):
+            if following == f"I-{current[2:]}":
+                return current
+            if following == "O" or following.startswith("B-") or following != f"I-{current[2:]}":
+                return f"U-{current[2:]}"
+            return current
+
+        if current.startswith("I-"):
+            if preceding != current and following == current:
+                return f"B-{current[2:]}"
+            if following == "O" or following.startswith("B-") or following != f"I-{current[2:]}":
+                if preceding in (current, f"B-{current[2:]}"):
+                    return f"L-{current[2:]}"
+                return f"U-{current[2:]}"
+            return f"I-{current[2:]}"
+
+        raise InvalidTagSequence(tags)
+
+    return list(
+        itertools.starmap(
+            convert,
+            zip(
+                itertools.chain("O", tags),
+                tags,
+                itertools.chain(tags[1:], "O"),
+            ),
+        )
+    )
+
+
+def bio_to_bioul(tags: Sequence[str]) -> List[str]:
+    def convert(preceding: str, current: str, following: str) -> str:
+        if current == "O":
+            return "O"
+
+        if current.startswith("B-"):
+            if following == f"I-{current[2:]}":
+                return current
+            if following == "O" or following.startswith("B-") or following != f"I-{current[2:]}":
+                return f"U-{current[2:]}"
+            return current
+
+        if current.startswith("I-"):
+            if following == "O" or following.startswith("B-") or following != f"I-{current[2:]}":
+                return f"L-{current[2:]}"
+            return f"I-{current[2:]}"
+
+        raise InvalidTagSequence(tags)
+
+    return list(
+        itertools.starmap(
+            convert,
+            zip(
+                itertools.chain("O", tags),
+                tags,
+                itertools.chain(tags[1:], "O"),
+            ),
+        )
+    )
+
+
+def bmes_to_bioul(tags: Sequence[str]) -> List[str]:
+    return [tag.replace("M-", "I-").replace("E-", "L-") for tag in tags]
+
+
+def bioul_to_iob1(tags: Sequence[str]) -> List[str]:
+    def convert(preceding: str, current: str, following: str) -> str:
+        if current == "O":
+            return "O"
+
+        if current.startswith(("B-", "U-")):
+            if preceding != "O" and preceding[2:] == current[2:]:
+                return f"B-{current[2:]}"
+            return f"I-{current[2:]}"
+
+        if current.startswith(("I-", "L-")):
+            return f"I-{current[2:]}"
+
+        raise InvalidTagSequence(tags)
+
+    return list(
+        itertools.starmap(
+            convert,
+            zip(
+                itertools.chain("O", tags),
+                tags,
+                itertools.chain(tags[1:], "O"),
+            ),
+        )
+    )
+
+
+def bioul_to_bio(tags: Sequence[str]) -> List[str]:
+    return [tag.replace("L-", "I-").replace("U-", "B-") for tag in tags]
+
+
+def bioul_to_bmes(tags: Sequence[str]) -> List[str]:
+    return [tag.replace("I-", "M-").replace("L-", "E-") for tag in tags]
+
+
+def bio_to_iob1(tags: Sequence[str]) -> List[str]:
+    return bioul_to_iob1(bio_to_bioul(tags))
+
+
+def iob1_to_bio(tags: Sequence[str]) -> List[str]:
+    return bioul_to_bio(iob1_to_bioul(tags))
+
+
+def bio_to_bmes(tags: Sequence[str]) -> List[str]:
+    return bioul_to_bmes(bio_to_bioul(tags))
+
+
+def bmes_to_bio(tags: Sequence[str]) -> List[str]:
+    return bioul_to_bio(bmes_to_bioul(tags))
+
+
+def iob1_to_bmes(tags: Sequence[str]) -> List[str]:
+    return bioul_to_bmes(iob1_to_bioul(tags))
+
+
+def bmes_to_iob1(tags: Sequence[str]) -> List[str]:
+    return bioul_to_iob1(bmes_to_bioul(tags))
+
+
+def tags_to_spans(tags: Sequence[str], encoding: LabelEncoding) -> List[LabelWithSpan]:
+    if encoding == "BIO":
+        return bio_tags_to_spans(tags)
+    if encoding == "BIOUL":
+        return bioul_tags_to_spans(tags)
+    if encoding == "BMES":
+        return bmes_tags_to_spans(tags)
+    if encoding == "IOB1":
+        return iob1_tags_to_spans(tags)
+    raise ValueError(f"Invalid encoding: {encoding}")
+
+
+def spans_to_tags(spans: Sequence[LabelWithSpan], length: int, encoding: LabelEncoding) -> List[str]:
+    if encoding == "BIO":
+        return spans_to_bio_tags(spans, length)
+    if encoding == "BIOUL":
+        return spans_to_bioul_tags(spans, length)
+    if encoding == "BMES":
+        return spans_to_bmes_tags(spans, length)
+    if encoding == "IOB1":
+        return spans_to_iob1_tags(spans, length)
+    raise ValueError(f"Invalid encoding: {encoding}")
+
+
+def convert_encoding(
+    tags: Sequence[str],
+    source_encoding: LabelEncoding,
+    target_encoding: LabelEncoding,
+) -> List[str]:
+    if source_encoding == target_encoding:
+        return list(tags)
+    if (source_encoding, target_encoding) == ("BIO", "BIOUL"):
+        return bio_to_bioul(tags)
+    if (source_encoding, target_encoding) == ("BIO", "BMES"):
+        return bio_to_bmes(tags)
+    if (source_encoding, target_encoding) == ("BIO", "IOB1"):
+        return bio_to_iob1(tags)
+    if (source_encoding, target_encoding) == ("BIOUL", "BIO"):
+        return bioul_to_bio(tags)
+    if (source_encoding, target_encoding) == ("BIOUL", "BMES"):
+        return bioul_to_bmes(tags)
+    if (source_encoding, target_encoding) == ("BIOUL", "IOB1"):
+        return bioul_to_iob1(tags)
+    if (source_encoding, target_encoding) == ("BMES", "BIO"):
+        return bmes_to_bio(tags)
+    if (source_encoding, target_encoding) == ("BMES", "BIOUL"):
+        return bmes_to_bioul(tags)
+    if (source_encoding, target_encoding) == ("BMES", "IOB1"):
+        return bmes_to_iob1(tags)
+    if (source_encoding, target_encoding) == ("IOB1", "BIO"):
+        return iob1_to_bio(tags)
+    if (source_encoding, target_encoding) == ("IOB1", "BIOUL"):
+        return iob1_to_bioul(tags)
+    if (source_encoding, target_encoding) == ("IOB1", "BMES"):
+        return iob1_to_bmes(tags)
+    raise ValueError(f"Invalid encoding pair: {source_encoding}, {target_encoding}")
