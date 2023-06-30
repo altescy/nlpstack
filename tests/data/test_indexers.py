4a886111d0ca59c52b653a5450592bf6792f653a
altescy <altescy@fastmail.com>
2023-06-30 13:23:12 +0900

    add fasttext indexer

---
+++ tests/data/test_indexers.py
@@ -0,0 +1,16 @@
+import numpy
+
+from nlpstack.data import Token, Vocabulary
+from nlpstack.data.token_indexers import PretrainedFasttextIndexer
+
+
+def test_pretrained_fasttext_indexer() -> None:
+    indexer = PretrainedFasttextIndexer("tests/fixtures/models/fasttext.bin")
+    tokens = [Token("Hello"), Token("World")]
+    # Currently, PretrainedFasttextIndexer does not use vocab.
+    output = indexer(tokens, Vocabulary())
+
+    assert set(output.keys()) == {"embeddings", "mask"}
+    assert isinstance(output["embeddings"], numpy.ndarray)
+    assert output["embeddings"].shape == (2, 100)
+    assert output["mask"].tolist() == [True, True]
