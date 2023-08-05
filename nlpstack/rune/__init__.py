e28fb6e8e83949176c149980bf2a66aae3e23b08
altescy <altescy@fastmail.com>
2023-08-05 15:32:52 +0900

    import SetupMode

--- nlpstack/rune/__init__.py
+++ nlpstack/rune/__init__.py
@@ -1,3 +1,3 @@
 from nlpstack.rune.archive import RuneArchive  # noqa: F401
-from nlpstack.rune.base import Rune  # noqa: F401
+from nlpstack.rune.base import Rune, SetupMode  # noqa: F401
 from nlpstack.rune.torch import RuneForTorch  # noqa: F401
