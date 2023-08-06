d17347cdc2a00e3566c6951ff614741d5765f461
altescy <altescy@fastmail.com>
2023-08-06 18:40:52 +0900

    add example of causal language modeling

---
+++ examples/causal_language_modeling/addition.py
@@ -0,0 +1,33 @@
+import random
+from typing import List
+
+from nlpstack.data.tokenizers import CharacterTokenizer
+from nlpstack.tasks.causal_language_modeling.sklearn import SklearnCausalLanguageModel
+
+
+def generate_dataset(num_examples: int) -> List[str]:
+    dataset: List[str] = []
+    for _ in range(num_examples):
+        a = random.randint(0, 100)
+        b = random.randint(0, 100)
+        text = f"{a} + {b} = {a + b}"
+        dataset.append(text)
+    return dataset
+
+
+X = generate_dataset(1000)
+model = SklearnCausalLanguageModel(
+    tokenizer=CharacterTokenizer(),
+    eos_token="@@END@@",
+    max_epochs=50,
+    learning_rate=1e-2,
+    batch_size=32,
+).fit(X)
+
+X_pred = [text.split(" = ")[0] + " = " for text in X[:10]]
+y_pred = model.predict(X_pred, temperature=0.0)
+
+for given, pred, gold in zip(X_pred, y_pred, X):
+    gold = gold[len(given) :]
+    pred = "".join(pred)
+    print(f"{pred} ({gold})")
