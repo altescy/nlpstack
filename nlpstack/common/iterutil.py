36aaaee9218509fb346ee33e5f1337360c422d61
altescy <altescy@fastmail.com>
2023-09-01 13:38:12 +0900

    add docstrings

--- nlpstack/common/iterutil.py
+++ nlpstack/common/iterutil.py
@@ -7,21 +7,41 @@ T = TypeVar("T")
 
 
 class SizedIterator(Generic[T]):
-    def __init__(self, it: Iterator[T], size: int):
-        self.it = it
+    """
+    A wrapper for an iterator that knows its size.
+
+    Args:
+        iterator: The iterator.
+        size: The size of the iterator.
+    """
+
+    def __init__(self, iterator: Iterator[T], size: int):
+        self.iterator = iterator
         self.size = size
 
     def __iter__(self) -> Iterator[T]:
-        return self.it
+        return self.iterator
 
     def __next__(self) -> T:
-        return next(self.it)
+        return next(self.iterator)
 
     def __len__(self) -> int:
         return self.size
 
 
-def batched(iterable: Iterable[T], batch_size: int) -> Iterator[List[T]]:
+def batched(iterable: Iterable[T], batch_size: int, drop_last: bool = False) -> Iterator[List[T]]:
+    """
+    Batch an iterable into lists of the given size.
+
+    Args:
+        iterable: The iterable.
+        batch_size: The size of each batch.
+        drop_last: Whether to drop the last batch if it is smaller than the given size.
+
+    Returns:
+        An iterator over batches.
+    """
+
     def iterator() -> Iterator[List[T]]:
         batch = []
         for item in iterable:
@@ -29,7 +49,7 @@ def batched(iterable: Iterable[T], batch_size: int) -> Iterator[List[T]]:
             if len(batch) == batch_size:
                 yield batch
                 batch = []
-        if batch:
+        if batch and not drop_last:
             yield batch
 
     if isinstance(iterable, abc.Sized):
@@ -40,6 +60,17 @@ def batched(iterable: Iterable[T], batch_size: int) -> Iterator[List[T]]:
 
 
 def batched_iterator(iterable: Iterable[T], batch_size: int) -> Iterator[Iterator[T]]:
+    """
+    Batch an iterable into iterators of the given size.
+
+    Args:
+        iterable: The iterable.
+        batch_size: The size of each batch.
+
+    Returns:
+        An iterator over batches.
+    """
+
     def iterator() -> Iterator[Iterator[T]]:
         stop = False
         batch_progress = 0
@@ -81,6 +112,17 @@ def iter_with_callback(
     iterable: Iterable[T],
     callback: Callable[[T], Any],
 ) -> Iterator[T]:
+    """
+    Iterate over an iterable and call a callback for each item.
+
+    Args:
+        iterable: The iterable.
+        callback: The callback to call for each item.
+
+    Returns:
+        An iterator over the iterable.
+    """
+
     def iterator() -> Iterator[T]:
         for item in iterable:
             yield item
