f585574de8acfe3253d116e85a5b85bfc6a8b6ca
altescy <altescy@fastmail.com>
2023-09-21 23:01:26 +0900

    add preprocessor/postprocessor options

--- nlpstack/tasks/classification/datamodules.py
+++ nlpstack/tasks/classification/datamodules.py
@@ -1,9 +1,9 @@
 from logging import getLogger
-from typing import Any, Dict, Iterable, Iterator, List, Mapping, Optional, Sequence
+from typing import Any, Callable, Dict, Iterable, Iterator, List, Mapping, Optional, Sequence
 
 import numpy
 
-from nlpstack.common import FileBackendSequence, ProgressBar
+from nlpstack.common import FileBackendSequence, PassThroughPipeline, ProgressBar
 from nlpstack.data import DataModule, Instance, Token, Vocabulary
 from nlpstack.data.fields import Field, LabelField, MetadataField, TextField
 from nlpstack.data.indexers import SingleIdTokenIndexer, TokenIndexer
@@ -12,6 +12,9 @@ from nlpstack.data.tokenizers import Tokenizer, WhitespaceTokenizer
 from .types import ClassificationExample, ClassificationInference, ClassificationPrediction
 
 logger = getLogger(__name__)
+
+ClassificationPreprocessor = Callable[[Iterable[ClassificationExample]], Iterable[ClassificationExample]]
+ClassificationPostprocessor = Callable[[Iterable[ClassificationPrediction]], Iterable[ClassificationPrediction]]
 
 
 class BasicClassificationDataModule(
@@ -32,6 +35,10 @@ class BasicClassificationDataModule(
         label_namespace: The vocabulary namespace for the labels. Defaults to `"labels"`.
         labels: The set of labels. If not given, the labels will be collected from the
             training dataset. Defaults to `None`.
+        preprocessors: The preprocessors to apply to the dataset before tokenization.
+            Defaults to `None`.
+        postprocessors: The postprocessors to apply to the predictions after inference.
+            Defaults to `None`.
     """
 
     def __init__(
@@ -41,12 +48,16 @@ class BasicClassificationDataModule(
         token_indexers: Optional[Mapping[str, TokenIndexer]] = None,
         label_namespace: str = "labels",
         labels: Optional[Sequence[str]] = None,
+        preprocessor: Optional[ClassificationPreprocessor] = None,
+        postprocessor: Optional[ClassificationPostprocessor] = None,
     ) -> None:
         self._vocab = vocab
         self._tokenizer = tokenizer or WhitespaceTokenizer()
         self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
         self._label_namespace = label_namespace
         self._labels = sorted(set(labels)) if labels else None
+        self._preprocessor = preprocessor or PassThroughPipeline[ClassificationExample]()
+        self._postprocessor = postprocessor or PassThroughPipeline[ClassificationPrediction]()
 
     @property
     def vocab(self) -> Vocabulary:
@@ -78,7 +89,7 @@ class BasicClassificationDataModule(
         """
         if dataset:
             logger.info("Tokenizing dataset and building vocabulary...")
-            dataset = self.tokenize(ProgressBar(dataset, desc="Tokenizing dataset"))
+            dataset = self.tokenize(ProgressBar(self._preprocessor(dataset), desc="Tokenizing dataset"))
             self._build_vocab(dataset)
 
     def tokenize(self, dataset: Iterable[ClassificationExample]) -> Sequence[ClassificationExample]:
@@ -166,21 +177,24 @@ class BasicClassificationDataModule(
             The predictions.
         """
 
-        sorted_indices = inference.probs.argsort(axis=1)[:, ::-1]
-        sorted_probs = numpy.take_along_axis(inference.probs, sorted_indices, axis=1)
-        for i, (top_indices, top_probs) in enumerate(zip(sorted_indices.tolist(), sorted_probs.tolist())):
-            num_labels_to_return = len(top_indices)
-            if inference.threshold is not None:
-                num_labels_to_return = sum(prob >= inference.threshold for prob in top_probs)
-            if inference.top_k is not None:
-                num_labels_to_return = min(num_labels_to_return, inference.top_k)
-            top_indices = top_indices[:num_labels_to_return]
-            top_probs = top_probs[:num_labels_to_return]
-            yield ClassificationPrediction(
-                top_probs=top_probs,
-                top_labels=[self.vocab.get_token_by_index(self.label_namespace, index) for index in top_indices],
-                metadata=inference.metadata[i] if inference.metadata is not None else None,
-            )
+        def prediction_iterator() -> Iterator[ClassificationPrediction]:
+            sorted_indices = inference.probs.argsort(axis=1)[:, ::-1]
+            sorted_probs = numpy.take_along_axis(inference.probs, sorted_indices, axis=1)
+            for i, (top_indices, top_probs) in enumerate(zip(sorted_indices.tolist(), sorted_probs.tolist())):
+                num_labels_to_return = len(top_indices)
+                if inference.threshold is not None:
+                    num_labels_to_return = sum(prob >= inference.threshold for prob in top_probs)
+                if inference.top_k is not None:
+                    num_labels_to_return = min(num_labels_to_return, inference.top_k)
+                top_indices = top_indices[:num_labels_to_return]
+                top_probs = top_probs[:num_labels_to_return]
+                yield ClassificationPrediction(
+                    top_probs=top_probs,
+                    top_labels=[self.vocab.get_token_by_index(self.label_namespace, index) for index in top_indices],
+                    metadata=inference.metadata[i] if inference.metadata is not None else None,
+                )
+
+        yield from self._postprocessor(prediction_iterator())
 
     def read_dataset(self, dataset: Iterable[ClassificationExample], **kwargs: Any) -> Iterator[Instance]:
         """
@@ -194,5 +208,5 @@ class BasicClassificationDataModule(
         """
 
         logger.info("Building instances...")
-        for example in ProgressBar(dataset, desc="Building instances"):
+        for example in ProgressBar(self._preprocessor(dataset), desc="Building instances"):
             yield self.build_instance(example)
